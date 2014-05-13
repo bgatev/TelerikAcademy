e065cf970ad1d2c7a6972ed4667597cdc0b06ec9
Boyko Gatev <bgatev@abv.bg>
2014-05-13 22:24:02 +0300

    JS Lection4

---
+++ JavaScript/Lection 4/numbers-to-n-not37.js
@@ -0,0 +1,8 @@
+﻿function NumbersToNNot37(n) {
+    for (var i = 1; i <= n; i++) {
+        if (i % 21 != 0) console.log(i);
+    }
+}
+
+var numberN = 134;
+NumbersToNNot37(numberN);
---
+++ JavaScript/Lection 4/numbers-to-n.js
@@ -0,0 +1,8 @@
+﻿function NumbersToN(n) {
+    for (var i = 1; i <= n; i++) {
+        console.log(i);
+    }
+}
+
+var numberN = 34;
+NumbersToN(numberN);
