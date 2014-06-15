761ffc22b7a75fcc547a95c29a4573df0ce9e80b
Boyko Gatev <bgatev@abv.bg>
2014-06-15 15:01:38 +0300

    JS 2 Lection 10

---
+++ JavaScript/JavaScript UI DOM/Lection 10/Lection 10/dropdown.js
@@ -0,0 +1,41 @@
+﻿/// <reference path="E:\Downloads\Telerik\JavaScript\JavaScript UI DOM\Lection 10\Lection 10\scripts/jquery-1.11.1.js" />
+$.fn.dropdown = function () {
+    var $this = this;
+    var $container = $this;
+    var $divContainer = $('<div />');
+    var $ulContainer = $('<ul />');
+    var $liTemplate = $('<li />');
+
+    $divContainer.addClass('dropdown-list-container');
+    $ulContainer.addClass('dropdown-list-options');
+    $liTemplate.addClass('dropdown-list-option');
+    $container.css('display', 'none');
+
+    var $options = $this.find('option');
+    console.log($options);
+
+    for (var i = 0; i < $options.length; i++) {
+        var $currentLI = $liTemplate.clone(true);
+        var currentLIText = $options[i].innerHTML;
+
+        $currentLI.attr('data-value', i);
+        $currentLI.html(currentLIText);
+        $currentLI.appendTo($ulContainer);
+    }
+
+    $ulContainer.appendTo($divContainer);
+    $container.parent().append($divContainer);
+
+    $(".dropdown-list-option").on("click", function () {
+        if ($(this).attr('selected') == 'selected') {
+            $(this).removeAttr('selected');
+            $(this).css('background-color', '');
+        }
+        else {
+            $(this).attr('selected', 'selected');
+            $(this).css('background-color', 'red');
+        }
+    });
+
+    return $this;
+};
---
+++ JavaScript/JavaScript UI DOM/Lection 10/Lection 10/message-box.js
@@ -0,0 +1,37 @@
+﻿/// <reference path="E:\Downloads\Telerik\JavaScript\JavaScript UI DOM\Lection 10\Lection 10\scripts/jquery-1.11.1.js" />
+$.fn.messageBox = function () {
+    var $this = $(this);
+
+    return {
+        success: function (message) {
+            $this.each(function () {
+                $this.css('background-color', 'green');
+                $this.animate({ 'opacity': 1 }, 1000);
+                $(this).html(message);
+                setTimeout(function () { $this.animate({ 'opacity': 0 }, 1000); }, 3000);
+            });
+
+            return $this;
+        },
+        error: function (message) {
+            $this.each(function () {
+                $this.css('background-color', 'red');
+                $this.animate({ 'opacity': 1 }, 1000);
+                $(this).html(message);
+                setTimeout(function () { $this.animate({ 'opacity': 0 }, 1000); }, 3000);
+            });
+
+            return $this;
+        }
+    };
+};
+
+var onSuccessButton = $('#successBtn').on('click', function () {
+    var msgBox = $('#message-box').messageBox();
+    msgBox.success('Success message');
+});
+
+var onErrorButton = $('#errorBtn').on('click', function () {
+    var msgBox = $('#message-box').messageBox();
+    msgBox.error('Error message');
+});
