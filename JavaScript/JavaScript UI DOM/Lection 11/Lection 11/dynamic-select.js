32a846c999f0aefe2416d2cddc2556aeb874e384
Boyko Gatev <bgatev@abv.bg>
2014-06-15 21:02:14 +0300

    JS 2 Lection 11

---
+++ JavaScript/JavaScript UI DOM/Lection 11/Lection 11/dynamic-select.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    var allItems = [
+        {
+            value: 1,
+            text: 'One'
+        },
+        {
+            value: 2,
+            text: 'Two'
+        },
+        {
+            value: 3,
+            text: 'Three'
+        },
+        {
+            value: 4,
+            text: 'Four'
+        }
+    ];
+
+
+    var post = { items: allItems };
+    var htmlTemplate = document.getElementById("items-template").innerHTML;
+    var postTemplate = Handlebars.compile(htmlTemplate);
+
+    var container = document.getElementById("mainContainer");
+    container.innerHTML = postTemplate(post);
+}());
---
+++ JavaScript/JavaScript UI DOM/Lection 11/Lection 11/list-view.js
@@ -0,0 +1,115 @@
+﻿/// <reference path="E:\Downloads\Telerik\JavaScript\JavaScript UI DOM\Lection 11\Lection 11\scripts/jquery-1.11.1.js" />
+(function ($) {
+    $.fn.listview = function (listViewItems) {
+        var $this = $(this);
+
+        var scriptID = $this.attr('data-template');
+
+        var htmlTemplate;
+        if (scriptID) htmlTemplate = document.getElementById(scriptID).innerHTML;
+        else htmlTemplate = document.getElementById('students-table-body').innerHTML;
+
+        console.log(htmlTemplate);
+
+        var postTemplate = Handlebars.compile(htmlTemplate);
+
+        $this.append(postTemplate(listViewItems));
+
+        return $this;
+    }
+}(jQuery));
+
+function createTable(insertIntoElement) {
+    var $table = $('<table />');
+    var $thead = $('<thead />');
+    var $tbody = $('<tbody />');
+    var $tr = $('<tr />');
+    var $thNum = $('<th />');
+    var $thName = $('<th />');
+    var $thMark = $('<th />');
+    var $trBody = $('<tr />');
+    var $tdNum = $('<td />');
+    var $tdName = $('<td />');
+    var $tdMark = $('<td />');
+
+    $tbody.attr('id','students-table-body');
+    $trBody.addClass('student-row');
+
+    $thNum.html('#');
+    $thName.html('Name');
+    $thMark.html('Mark');
+
+    $tdNum.html('{{number}}');
+    $tdName.html('{{name}}');
+    $tdMark.html('{{mark}}');
+
+    $thNum.appendTo($tr);
+    $thName.appendTo($tr);
+    $thMark.appendTo($tr);
+
+    $tr.appendTo($thead);
+    $thead.appendTo($table);
+
+    $tbody.append('{{#each students}}');
+
+    $tdNum.appendTo($trBody);
+    $tdName.appendTo($trBody);
+    $tdMark.appendTo($trBody);
+
+    $trBody.appendTo($tbody);
+    $tbody.append('{{/each}}');
+  
+    $tbody.appendTo($table);   
+    $table.appendTo(document.getElementById(insertIntoElement));
+}
+
+var allBooks = {
+    books: [
+            {
+                id: 1,
+                title: 'JavaScript: The Good parts'
+            },
+            {
+                id: 2,
+                title: 'Secrets of the JavaScript Ninja'
+            },
+            {
+                id: 3,
+                title: 'Core HTML5 Canvas'
+            },
+            {
+                id: 4,
+                title: 'JavaScript Patterns'
+            }
+    ]
+};
+
+var allStudents = {
+    students: [
+            {
+                number: 1,
+                name: 'Peter Petrov',
+                mark: 5.5
+            },
+            {
+                number: 2,
+                name: 'Stamat Georgiev',
+                mark: 4.7
+            },
+            {
+                number: 3,
+                name: 'Maria Todorova',
+                mark: 6
+            },
+            {
+                number: 4,
+                name: 'Georgi Geshov',
+                mark: 3.7
+            },
+            {
+                number: 5,
+                name: 'Anna Hristova',
+                mark: 4
+            }
+    ]
+};
